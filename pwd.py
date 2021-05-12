6f6041d03e4ff45d933ed050db6da9958abb7963
Joy Wang <dreamervul21@gmail.com>
2021-05-12 21:58:26 +0800

    basic version

--- pwd.py
+++ pwd.py
@@ -1,12 +1,13 @@
+# yjo4
 password = 'a123456'
-x = 3 #剩餘機會
+i = 3
 while True:
-	pwd = input('請輸入密碼：')
+	pwd = input('請輸入密碼:')
 	if pwd == password:
 		print('登入成功！')
-		break #逃出迴圈
+		break
 	else:
-		x = x - 1
-		print('密碼錯誤！還有', x ,'次機會')
-		if x == 0:
+		i = i - 1
+		print('密碼錯誤！還有', i,'次機會')
+		if i == 0:
 			break
